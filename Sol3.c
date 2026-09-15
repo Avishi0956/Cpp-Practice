c79faffc356422139605f534d41a3169ad282f00
Avishi Sharma <avishisharma30@gmail.com>
2026-09-15 23:48:22 +0530

    Added  solution 3

---
+++ Sol3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+
+    for(i=1; i<=n; i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d\t",i);
+        }
+
+    }
+}
+
+
+
