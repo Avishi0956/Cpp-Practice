95fe4990e1160b266445b2211809dc676ffdc55f
Avishi Sharma <avishisharma30@gmail.com>
2026-09-20 17:03:26 +0530

    Adding solution 5

---
+++ Sol5.c
@@ -0,0 +1,19 @@
+int main()
+{
+    int n,i, sum=0;
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+
+
+    for(i=1;i<=n;i++)
+    {
+         sum=sum+i;
+    }
+
+
+    printf("%d",sum);
+
+
+
+}
+
